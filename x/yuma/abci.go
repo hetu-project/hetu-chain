a8c075bb0ae90315282b4cdde7a5594f901c3bc5
maoaixiao1314 <m18634441821@163.com>
2025-07-11 11:12:58 +0800

    Modify some configurations and implement abci

--- x/yuma/abci.go
+++ x/yuma/abci.go
@@ -6,7 +6,13 @@ import (
 )
 
 // EndBlock 在每个区块结束时运行
-func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	// 简化的模块，暂时不需要复杂的区块结束处理
+/*
+这个文件实现了 Cosmos SDK 的 ABCI (Application Blockchain Interface) 接口：
+作用：
+EndBlock: 在每个区块结束时被调用，用于执行区块结束时的逻辑
+目前是简化实现，只返回空的验证者更新列表
+可以在这里添加区块结束时的清理工作、状态更新等
+*/
+func (am AppModule) EndBlock(ctx sdk.Context) []abci.ValidatorUpdate {
 	return []abci.ValidatorUpdate{}
 }
